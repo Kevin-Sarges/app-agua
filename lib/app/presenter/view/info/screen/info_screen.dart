c5b304016935b3c18209e578afe9fdc09a46c004
Kevin-Sarges <kevinsarges08@gmail.com>
2022-12-16 22:11:09 -0300

    iniciando tela de contato

--- lib/app/presenter/view/info/screen/info_screen.dart
+++ lib/app/presenter/view/info/screen/info_screen.dart
@@ -1,3 +1,5 @@
+import 'package:appaguaentregados/app/domain/utils/colors_app.dart';
+import 'package:appaguaentregados/app/presenter/global_widgets/button_app_widget.dart';
 import 'package:flutter/material.dart';
 
 class InfoScreen extends StatefulWidget {
@@ -8,8 +10,75 @@ class InfoScreen extends StatefulWidget {
 }
 
 class _InfoScreenState extends State<InfoScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Nome e Contato'),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 64,
+              ),
+              const Text(
+                'Informe seu nome número para o entregador entra em contato com você caso seja necessario',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(
+                height: 54,
+              ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          label: Text('Somente nome e sobrenome'),
+                          hintText: 'Ex: Kevin Sarges'),
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Número para contato'),
+                        hintText: 'Ex: 984279843',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ButtonAppWidget(
+                onPressed: () {},
+                text: const Text(
+                  'Salvar informações',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: ColorsApp.blueDarck,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 96,
+                    vertical: 9,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
