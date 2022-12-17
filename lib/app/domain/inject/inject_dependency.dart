323ecc07f6902cb36e88326ade33eb2915b2eb7b
Kevin-Sarges <kevinsarges08@gmail.com>
2022-12-16 22:09:31 -0300

    add nova injeção de dependencia

--- lib/app/domain/inject/inject_dependency.dart
+++ lib/app/domain/inject/inject_dependency.dart
@@ -1,10 +1,18 @@
+import 'package:appaguaentregados/app/data/datasoucer/dbfirbase_interface.dart';
+import 'package:appaguaentregados/app/data/services/service_firebase.dart';
 import 'package:appaguaentregados/app/presenter/view/home/inject/home_inject.dart';
+import 'package:appaguaentregados/app/presenter/view/info/inject/info_Inject.dart';
 import 'package:get_it/get_it.dart';
 
 class InjectDependency {
   static void init() {
     final getIt = GetIt.instance;
 
+    getIt.registerSingleton<IDbFirebase>(
+      ServiceFirebase(),
+    );
+
     InjectDependencyHome.init(getIt);
+    InjectDependencyInfo.init(getIt);
   }
 }
