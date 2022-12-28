1c9a91a8ece1986bdf8248476cb246e4e41323df
Kevin-Sarges <kevinsarges08@gmail.com>
2022-12-27 21:18:31 -0300

    atualização na tela de onboarding

--- lib/app/presenter/view/onboarding/screen/onboarding_screen.dart
+++ lib/app/presenter/view/onboarding/screen/onboarding_screen.dart
@@ -1,6 +1,6 @@
 import 'package:appaguaentregados/app/domain/utils/colors_app.dart';
 import 'package:appaguaentregados/app/domain/utils/routes_app.dart';
-import 'package:appaguaentregados/app/presenter/view/onboarding/widget/text_onboarding.dart';
+import 'package:appaguaentregados/app/presenter/view/onboarding/widget/info_app_widget.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingScreen extends StatefulWidget {
@@ -10,76 +10,49 @@ class OnboardingScreen extends StatefulWidget {
   State<OnboardingScreen> createState() => _OnboardingScreenState();
 }
 
-class _OnboardingScreenState extends State<OnboardingScreen> {
+class _OnboardingScreenState extends State<OnboardingScreen>
+    with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: ColorsApp.bluePrimary,
-        body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Dicas',
-                style: TextStyle(
-                  color: ColorsApp.white,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              Image.asset(
-                'assets/images/garrafa-de-agua.png',
-                width: 100,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                width: 200,
-                child: Column(
-                  children: [
-                    TextOnboarding(
-                      text:
-                          '1. O app precisa de permissão para acessar a sua localização então não se esqueça de verificar a sua localização quando for comprar água no app.',
-                    ),
-                    const SizedBox(height: 10),
-                    TextOnboarding(
-                      text:
-                          '2. Informe seu nome e número, o entregador entrará em contato com você caso seja necessário.',
-                    ),
-                    const SizedBox(height: 10),
-                    TextOnboarding(
-                      text:
-                          '3. Caso tenha algum problema ou demora na entrega, você pode entra em contato direto com o fornecedor.',
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              Container(
-                width: 55,
-                height: 55,
-                decoration: BoxDecoration(
-                  color: ColorsApp.blueDarck,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: IconButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, RoutesApp.home);
-                  },
-                  icon: const Icon(Icons.arrow_forward_ios),
-                  color: ColorsApp.white,
-                ),
-              )
-            ],
-          ),
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            InfoAppWidget(
+              title: '1ª Dica',
+              text:
+                  'O app precisa de permissão para acessar a sua localização então não se esqueça de verificar a sua localização quando for comprar água no app.',
+              image: 'assets/images/map.png',
+              onPress: null,
+            ),
+            InfoAppWidget(
+              title: '2ª Dica',
+              text:
+                  'Informe seu nome e número, o entregador entrará em contato com você caso seja necessário.',
+              image: 'assets/images/user.png',
+              onPress: null,
+            ),
+            InfoAppWidget(
+              title: '3ª Dica',
+              text:
+                  'Caso tenha algum problema ou demora na entrega, você pode entra em contato direto com o fornecedor.',
+              image: 'assets/images/garrafa-de-agua.png',
+              onPress: () {
+                Navigator.pushNamed(context, RoutesApp.home);
+              },
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/app/presenter/view/onboarding/widget/info_app_widget.dart
@@ -0,0 +1,74 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:appaguaentregados/app/domain/utils/colors_app.dart';
+import 'package:appaguaentregados/app/presenter/view/onboarding/widget/text_onboarding.dart';
+import 'package:flutter/material.dart';
+
+class InfoAppWidget extends StatelessWidget {
+  InfoAppWidget({
+    Key? key,
+    required this.text,
+    required this.title,
+    required this.image,
+    required this.onPress,
+  }) : super(key: key);
+
+  String text;
+  String title;
+  String image;
+  VoidCallback? onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                color: ColorsApp.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Image.asset(
+              image,
+              width: 100,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: 200,
+              child: TextOnboarding(
+                text: text,
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Container(
+              width: 55,
+              height: 55,
+              decoration: BoxDecoration(
+                color: onPress == null ? Colors.grey : ColorsApp.blueDarck,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: IconButton(
+                onPressed: onPress,
+                icon: const Icon(Icons.arrow_forward_ios),
+                color: ColorsApp.white,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
