9f0ad49115c62955a3010906bf21f7e847bd70db
Kevin-Sarges <kevinsarges08@gmail.com>
2023-01-05 17:37:30 -0300

    ajustes

--- lib/app/features/onboarding/presenter/screen/onboarding_screen.dart
+++ lib/app/features/onboarding/presenter/screen/onboarding_screen.dart
@@ -1,6 +1,7 @@
 import 'package:appaguaentregados/app/common/utils/colors_app.dart';
 import 'package:appaguaentregados/app/common/utils/routes_app.dart';
 import 'package:appaguaentregados/app/features/onboarding/presenter/widget/info_app_widget.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingScreen extends StatefulWidget {
@@ -26,31 +27,40 @@ class _OnboardingScreenState extends State<OnboardingScreen>
     return SafeArea(
       child: Scaffold(
         backgroundColor: ColorsApp.bluePrimary,
-        body: TabBarView(
-          controller: _tabController,
+        body: Column(
           children: [
-            InfoAppWidget(
-              title: '1ª Dica',
-              text:
-                  'O app precisa de permissão para acessar a sua localização então não se esqueça de verificar a sua localização quando for comprar água no app.',
-              image: 'assets/images/map.png',
-              onPress: null,
-            ),
-            InfoAppWidget(
-              title: '2ª Dica',
-              text:
-                  'Informe seu nome e número, o entregador entrará em contato com você caso seja necessário.',
-              image: 'assets/images/user.png',
-              onPress: null,
-            ),
-            InfoAppWidget(
-              title: '3ª Dica',
-              text:
-                  'Caso tenha algum problema ou demora na entrega, você pode entra em contato direto com o fornecedor.',
-              image: 'assets/images/garrafa-de-agua.png',
-              onPress: () {
-                Navigator.pushNamed(context, RoutesApp.home);
-              },
+            Expanded(
+              child: Container(
+                child: TabBarView(
+                  controller: _tabController,
+                  dragStartBehavior: DragStartBehavior.down,
+                  children: [
+                    InfoAppWidget(
+                      title: '1ª Dica',
+                      text:
+                          'O app precisa de permissão para acessar a sua localização então não se esqueça de verificar a sua localização quando for comprar água no app.',
+                      image: 'assets/images/map.png',
+                      onPress: () {},
+                    ),
+                    InfoAppWidget(
+                      title: '2ª Dica',
+                      text:
+                          'Informe seu nome e número, o entregador entrará em contato com você caso seja necessário.',
+                      image: 'assets/images/user.png',
+                      onPress: () {},
+                    ),
+                    InfoAppWidget(
+                      title: '3ª Dica',
+                      text:
+                          'Caso tenha algum problema ou demora na entrega, você pode entra em contato direto com o fornecedor.',
+                      image: 'assets/images/garrafa-de-agua.png',
+                      onPress: () {
+                        Navigator.pushNamed(context, RoutesApp.home);
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
